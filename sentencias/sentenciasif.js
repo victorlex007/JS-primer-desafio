f252e0debf60554e5a3d8513803c2ff8007f0b0a
victorlex007 <103323257+victorlex007@users.noreply.github.com>
2022-05-30 23:50:42 -0300

    Add files via upload

---
+++ sentencias/sentenciasif.js
@@ -0,0 +1,102 @@
+//let nombreCompleto = "Victor Lezcano"
+//let respuesta = (1 == 1)
+
+//debugger
+//let userName = "Coderhouse"
+
+//if (userName == "Coderhouse"){
+
+//    console.log("Bienvenido", userName)
+    
+//} 
+
+//condiciones anidadas
+
+//let miOferta = parseInt(prompt("Ingrese su oferta economica"))
+
+//debugger
+
+//miOferta = parseInt(miOferta)
+
+//if (miOferta < 20) {
+ //   console.error ("No Way! Ni Cerca.")
+//}
+
+//debugger
+//let respuesta = confirm ("Desea formatear el disco?")
+
+//if (respuesta == true) {
+//    console.warn("El disco se formateo")
+//} else{
+//   console.log ("Sus archivos estan intactos")
+//}
+
+// debugger
+// let userName = prompt ("Ingrese su nombre de usuario") {
+//     if (userName == "CoderHouse")    
+//     }
+//     console.log("Bienvenido a BankApp");
+//     else {
+//         console.warn("Usuario no encontrado")
+//     }
+
+//CICLOS POR CONTEO
+//conteo del 0 al 21
+
+//ParseINt: cadena (string) y convierte a nro entero
+//ParseFloat: cadena (string) y convierte a nro decimal
+
+// debugger
+
+// let limite = parseInt(prompt("Ingrese un numero:"))
+
+// for (let i = 0; i < limite; i++) {
+// console.log ("Conteo", i)
+// if(i === 500){
+//     break
+// }
+// }
+
+// debugger
+
+// let Seguimos = true
+
+// while (Seguimos) {
+//     console.log ("Una nueva iteracion.")
+//     Seguimos = confirm("Desea seguir loopeando?")
+// }
+
+// let color = prompt("Ingrese color de la prenda que busca")
+
+// switch (color) {
+//     case "negro":
+//         console.log("Tenemos stock en", color)
+//         break;
+// case "amarillo":
+//     console.log("No tenemos stock en", color)
+//     default:
+//         break;
+// }
+
+// let PalabraSecreta = "Hola"
+// prompt ("Ingrese la palabra secreta")
+// if (PalabraSecreta == "Hola"){
+//     console.log("Palabra secreta correcta");
+// } else {
+//     alert("No dijiste la palabra secreta!!!!");
+// }
+// let PalabraSecreta = "Hola"
+// prompt ("Ingrese palabra secreta")
+// if (PalabraSecreta == "Hola") {
+//     console.log("Palabra secreta correcta");
+//   } else {
+//     alert("No dijiste la palabra secreta!!!!")
+//   }
+
+  let PalabraSecreta = prompt ("Ingrese palabra secreta");
+
+  if(PalabraSecreta !="Hola") && ((PalabraSecreta == "Hola") || (PalabraSecreta == "hola"));{
+      alert("Palabra secreta correcta");
+  }else{
+      alert("Error: no dijiste la palabra secreta")
+  }
